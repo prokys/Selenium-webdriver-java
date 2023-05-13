a184e3f7d27ac00714860591891df32fb3d91040
Prokop Ferdan <prokes1@gmail.com>
2023-05-13 12:45:26 +0200

    Adding chapter 4.1 code

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,40 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
+        homePage = new HomePage(driver);
 
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
-
-        System.out.println(driver.getTitle());
         driver.quit();
     }
-    public void chapter3Exercise(){
-        driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContent.click();
-        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
-        example1.click();
-        List<WebElement> listOfElements = driver.findElements(By.tagName("li"));
-        System.out.println(listOfElements.size());
-        driver.quit();
-
-    }
+//    public void chapter3Exercise(){
+//        driver = new ChromeDriver();
+//        driver.get("https://the-internet.herokuapp.com/");
+//        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+//        shiftingContent.click();
+//        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+//        example1.click();
+//        List<WebElement> listOfElements = driver.findElements(By.tagName("li"));
+//        System.out.println(listOfElements.size());
+//        driver.quit();
+//    }
     public static void main(String args[]){
         BaseTests test = new BaseTests();
-        test.chapter3Exercise();
+        test.setUp();
     }
 }
 
